4968cbbae309757ce249d1d1ed1e5ed61242ce72
hussainali225 <armanrayyan225@gmail.com>
2024-12-17 00:06:08 +0500

    Add files via upload

---
+++ index.html/hussain.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+// chapter#1
+
+// chapter 1
+alert(" I have some information for you  ");
+
+// chapter 2
+var email = "hussainali202420252026@gmail.com";
+alert(email);
+
+
+
+// chapter 3
+var string = "Education";
+
+var string2 = "\n Matric";
+alert(string  + string2);
+
+
+Swal.fire({
+  position: "top-end",
+  icon: "success",
+  title: "Your work has been saved",
+  showConfirmButton: false,
+  timer: 1500
+});
+
